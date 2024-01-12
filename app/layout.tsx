7af48a0a6c113a70072ec8d0476b2e0ff1aadcde
parshva555 <parshvadani@gmail.com>
2024-01-12 19:23:02 +0530

    Header outline

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import Header from "@/components/header";
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -15,10 +15,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]`}>
 
       <div className='bg-[#d2e6ec] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
       <div className='bg-[#f3e8ee] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <Header/>
         {children}
       
       </body>
