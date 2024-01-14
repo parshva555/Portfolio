59178d61bd318cf30b45f16aee4e413ce86f5831
parshva555 <parshvadani@gmail.com>
2024-01-15 00:33:43 +0530

    Completing Intro

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,10 @@
 import React from 'react'
 import Image from 'next/image'
 import { motion } from "framer-motion"
+import Link from 'next/link'
+import {BsArrowRight, BsLinkedin} from 'react-icons/bs'
+import {HiDownload} from 'react-icons/hi'
+import { FaGithubSquare } from 'react-icons/fa'
 export default function Intro() {
   return (
     <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
@@ -39,6 +43,12 @@ export default function Intro() {
             building <span className="italic">websites & apps</span>. I am currently learning{" "}
             <span className="underline">React (Next.js)</span>.
         </motion.p>
+        <motion.div className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium' initial={{ opacity: 0, y: 100 }} animate={{ opacity: 1, y: 0 }} transition={{delay: 0.1}}>
+            <Link href="#contact" className='bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition'>Contact me here <BsArrowRight/></Link>
+            <a href="CV.pdf" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"> Download CV <HiDownload className="opacity-60 group-hover:translate-y-1 transition"/></a>
+            <a href="https://www.linkedin.com/in/parshva-dani-28551b24b/"  target="_blank" className='bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'> <BsLinkedin/></a>
+            <a href="https://github.com/parshva555"  target="_blank" className='bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60'> <FaGithubSquare/></a>
+        </motion.div>
     </section>
   )
 }
