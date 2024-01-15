e5333ca3b3ef368484cb3487ba33b4022b534326
parshva555 <parshvadani@gmail.com>
2024-01-15 21:51:22 +0530

    Mapping up project details from data.ts

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import About from "@/components/about";
 import Intro from "@/components/intro";
+import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Intro/>
       <SectionDivider/>
       <About/>
+      <Projects/>
     </main>
   )
 }
---
+++ components/projects.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import SectionHeading from './section-heading'
+import { projectsData } from '@/lib/data'
+
+export default function Projects() {
+  return (
+    <section>
+        <SectionHeading>My Projects</SectionHeading>
+        <div>
+            {
+                projectsData.map((project,index)=> (
+                    <React.Fragment key={index}>
+                    <Project {...project}/>
+                    </React.Fragment>
+                    
+                ))
+            }
+        </div>
+    </section>
+  )
+}
+
+type ProjectProps = typeof projectsData[number];
+function Project({
+    title,description,tags,imageUrl}:ProjectProps){
+    return (
+        <section>
+            <h3>{title}</h3>
+            <p>{description}</p>
+            {tags.map((tag,index) => (
+                <li key={index}>{tag}</li>
+            ))}
+        </section>
+    )
+}
