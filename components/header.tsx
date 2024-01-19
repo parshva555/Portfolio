d36520cb7b0ecc3fefae6c958e298e62ad1c40a4
parshva555 <parshvadani@gmail.com>
2024-01-19 10:29:29 +0530

    Adding active section navigation on home

--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,11 @@
 "use client"
-import React from 'react'
+import React, { useState } from 'react'
 import { motion } from "framer-motion"
 import {links} from "@/lib/data"
+import clsx from 'clsx'
 import Link from 'next/link'
 export default function () {
+  const [activeSection,setActiveSection] = useState('Home')
   return <header className='z-[999] relative'>
       <motion.div
         className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
@@ -14,11 +16,11 @@ export default function () {
           <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
             {
               links.map(link => (
-                <motion.li className="h-3/4 flex items-center justify-center relative"
+                <motion.li className="h-3/4 realtive flex items-center justify-center relative"
                 key={link.hash}
                 initial={{ y: -100, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}>
-                  <Link className='flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition ' href={link.hash}>{link.name}</Link>
+                  <Link className={clsx('flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition',{"text-gray-950":activeSection===link.name})} href={link.hash}>{link.name} {link.name === activeSection && (<span className='bg-gray-100 rounded-full absolute inset-0 -z-10'></span>) }</Link>
                 </motion.li>
               ))
             }
