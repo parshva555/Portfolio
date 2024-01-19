15f260ef8a7ff1fba6af23b237d1ca9708567263
parshva555 <parshvadani@gmail.com>
2024-01-19 12:00:05 +0530

    Context API

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from "@/components/header";
+import ActiveSectionContextProvider from '@/context/active-section-context';
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -19,8 +20,10 @@ export default function RootLayout({
 
       <div className='bg-[#d2e6ec] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
       <div className='bg-[#f3e8ee] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]'></div>
+        <ActiveSectionContextProvider>
         <Header/>
         {children}
+        </ActiveSectionContextProvider>
       
       </body>
     </html>
