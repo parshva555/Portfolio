f0ab8df6530848600e7326715f8d421839510279
parshva555 <parshvadani@gmail.com>
2024-01-20 20:21:58 +0530

    Setting up active sections

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,11 +1,23 @@
+"use client";
 
-import React, { useRef } from 'react'
+import React, { useRef,useEffect } from 'react'
 import SectionHeading from './section-heading'
+import { useInView } from 'react-intersection-observer';
+import { useActiveSectionContext } from '@/context/active-section-context';
 import { projectsData } from '@/lib/data'
 import Project from './project'
 export default function Projects() {
+  const{ref,inView}= useInView({
+    threshold:0.5
+  });
+  const {setActiveSection} = useActiveSectionContext();
+  useEffect(() => {
+    if(inView){
+      setActiveSection('Projects');
+    }
+  },[inView,setActiveSection])
   return (
-    <section id='projects' className='scroll-mt-28'>
+    <section ref={ref} id='projects' className='scroll-mt-28'>
         <SectionHeading>My Projects</SectionHeading>
         <div>
             {
