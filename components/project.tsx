c838d4f984e7d186199fb558c010628f6652fe29
parshva555 <parshvadani@gmail.com>
2024-02-03 22:21:32 +0530

    Making it responsive

--- components/project.tsx
+++ components/project.tsx
@@ -29,8 +29,8 @@ export default function Project({
     >
       <section className=" bg-gray-100 max-w-[42rem] border border-black/5 
       overflow-hidden sm:pr-8 relative sm:h-[20rem] mb-3 
-      rounded-lg sm:mb-8 last:mb-0  hover:bg-gray-200 dark:text-white transition group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full group-even:ml-[18rem]">
+      rounded-lg sm:mb-8 last:mb-0  hover:bg-gray-200 dark:text-white transition sm:group-even:pl-8 dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="pt-4 pb-7 px-5 sm:pl-10 sm:pr-2 sm:pt-10 sm:max-w-[50%] flex flex-col h-full sm:group-even:ml-[18rem]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">{description}</p>
           <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
@@ -48,7 +48,18 @@ export default function Project({
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
-          className="absolute top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.10] group-even:right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2    group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2"
+          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl
+          transition 
+          group-hover:scale-[1.04]
+          group-hover:-translate-x-3
+          group-hover:translate-y-3
+          group-hover:-rotate-2
+  
+          group-even:group-hover:translate-x-3
+          group-even:group-hover:translate-y-3
+          group-even:group-hover:rotate-2
+  
+          group-even:right-[initial] group-even:-left-40"
         />
       </section>
     </motion.div>
